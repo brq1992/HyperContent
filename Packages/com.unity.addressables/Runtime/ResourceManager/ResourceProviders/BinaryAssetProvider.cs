7e5a556d67c082cdfcfb79c855114dfcf53c34a4
jeff <jeff.ban@igg.com>
2026-01-20 15:56:48 -0800

    add local addressable package

---
+++ Packages/com.unity.addressables/Runtime/ResourceManager/ResourceProviders/BinaryAssetProvider.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel;
+using UnityEngine.ResourceManagement.Util;
+
+namespace UnityEngine.ResourceManagement.ResourceProviders
+{
+    /// <summary>
+    /// Converts ray binary data into the requested object.
+    /// </summary>
+    [DisplayName("Binary Asset Provider")]
+    internal class BinaryAssetProvider<TAdapter> : BinaryDataProvider where TAdapter : BinaryStorageBuffer.ISerializationAdapter, new()
+    {
+        /// <summary>
+        /// Converts raw bytes into requested object type via BinaryStorageBuffer.ISerializationAdapter
+        /// </summary>
+        /// <param name="type">The object type the text is converted to.</param>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>Returns the converted object.</returns>
+        public override object Convert(Type type, byte[] data)
+        {
+            try
+            {
+               return new BinaryStorageBuffer.Reader(data, 512, new TAdapter()).ReadObject(type, 0, false);
+            }
+            catch (Exception e)
+            {
+            // try
+            // {
+            //     // Added by Nick Zhao: Uncompress Catalog.json
+            //     data = SevenZip.SevenZipExtractor.ExtractBytes(data);
+            //     return new BinaryStorageBuffer.Reader(data, 512, new TAdapter()).ReadObject(type, 0, false);
+            // }
+            // catch (Exception e)
+            // {
+            //     return new BinaryStorageBuffer.Reader(data, 512, new TAdapter()).ReadObject(type, 0, false);
+                if (!IgnoreFailures)
+                       Debug.LogException(e);
+
+                   return null;
+            // }
+            }
+        }
+    }
+}
